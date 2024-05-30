e429d56d020ae73cc6f0679436cda188abe3b888
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2024-05-29 21:34:36 -0700

    install dependencies

---
+++ ecs170/app/page.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="overflow-hidden flex justify-center font-bold text-4xl">
+      <h1>Heart Disease Calculator</h1>
+    </main>
+  );
+}
