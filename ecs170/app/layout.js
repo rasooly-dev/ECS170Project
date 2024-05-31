d32938629f00979c659f1965d334892efc18e506
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2024-05-30 20:23:08 -0700

    Created landing page (non-responsive)

--- ecs170/app/layout.js
+++ ecs170/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-gradient-to-t from-rose-200 min-h-full pb-40`}>{children}</body>
     </html>
   );
 }
--- ecs170/app/page.js
+++ ecs170/app/page.js
@@ -1,8 +1,27 @@
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="overflow-hidden flex justify-center font-bold text-4xl">
-      <h1>Heart Disease Calculator</h1>
+    <main>
+        <div className='flex justify-between w-full h-full p-10'>
+          <div className='flex flex-col justify-center items-center mt-10'>
+            <h1 className='text-5xl font-hammersmith text-dark font-semibold text-center max-w-sm'>Heart Disease Calculator</h1>
+            <Image src="./heart.svg"
+                  alt="Heart Icon"
+                  width={100}
+                  height={100}
+                  className='w-3/4 h-fit'
+                />
+          </div>
+          <div className='flex flex-col items-center'>
+            <div className='bg-[#CC9090] p-4 pb-60 rounded-2xl mb-10'>
+              <h2 className='text-3xl font-hammersmith text-white font-medium pb-4'>About me!</h2>
+              <p className='font-hammersmith text-white text-lg font-medium max-w-2xl'>This app was created to help people predict their probability of Heart Disease.
+                  We do this by using.... short description!</p>
+            </div>
+            <button className='px-8 py-4 w-fit h-fit text-dark font-hammersmith font-semibold text-2xl rounded-xl shadow-lg bg-gray-600 bg-opacity-10 flex'>Start Survey</button>
+          </div>
+        </div>
     </main>
   );
 }
