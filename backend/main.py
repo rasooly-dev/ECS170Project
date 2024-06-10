9c3ed5a608aeee1c00b6940a91525b23263a5497
Christopher Chen <christopher.yi.chen@gmail.com>
2024-06-10 00:34:49 -0700

    help me

--- backend/main.py
+++ backend/main.py
@@ -4,15 +4,40 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import joblib
 import numpy as np
+import tensorflow as tf
+from keras.saving import register_keras_serializable
+import pickle
+import pandas as pd
 
+
+
+# @register_keras_serializable()
+@register_keras_serializable()
+def weighted_binary_crossentropy(weights):
+    @register_keras_serializable()
+    def loss(y_true, y_pred):
+        y_true = tf.cast(y_true, tf.float32)
+        weights_v = y_true * weights[1] + (1. - y_true) * weights[0]
+        return tf.keras.backend.binary_crossentropy(y_true, y_pred) * weights_v
+    return loss
+
+class_weights = {
+    0: 0.5519894511003669,
+    1: 5.308667810655841
+}
+
+custom_objects = {
+    'custom_loss_function': weighted_binary_crossentropy([class_weights[0], class_weights[1]])
+}
 
 # uvicorn main:app --reload
 # To reload the application
 
 app = FastAPI()
 
-# TODO: update this with actual model
-model = joblib.load("./Logistic_Regression.pkl")
+model = pickle.load(open('NN4withDropout.pkl', 'rb'))
+scalar = pickle.load(open('scaler.pkl', 'rb'))
+
 
 
 class HeartDiseaseInput(BaseModel):
@@ -34,9 +59,37 @@ def predict(data: HeartDiseaseInput):
         highBloodPressure = 1 if data.bloodPressure > 135 else 0
         highChol = 1 if data.chol > 240 else 0
 
-        input_data = np.array([[highBloodPressure, highChol, data.smoke, data.stroke, data.diabetes, data.physHealth, data.diffWalk, data.age]])
-        # bp, col, smoke, stroke
-        prediction = model.predict(input_data)
+        binary_features = np.array([[highBloodPressure, highChol, data.smoke, data.stroke, data.diabetes, data.diffWalk]])
+        non_binary_features = np.array([[data.physHealth, data.age]])
+        non_binary_features_scaled = scalar.transform(non_binary_features)
+        # input_data_scaled = np.concatenate([binary_features, non_binary_features_scaled], axis=1)
+        # HeartDiseaseorAttack    HighBP    HighChol    Smoker    Stroke    Diabetes    PhysHlth    DiffWalk    Age
+        input_data_scaled = np.concatenate([binary_features, non_binary_features_scaled], axis=1)
+
+        columns = ['highBloodPressure', 'highChol', 'smoke', 'stroke', 'diabetes', 'physHlth', 'diffWalk', 'age']
+
+        df = pd.DataFrame([np.array([binary_features["highBloodPressure"], binary_features["highChol"], binary_features["data.smoke"], binary_features["stroke"], binary_features["diabetes"], non_binary_features_scaled["physHealth"], binary_features["diffWalk"], non_binary_features["age"]])], columns=columns)
+
+        # df = pd.DataFrame(input_data_scaled, columns=columns)
+        
+        # df = df[['highBloodPressure', 'highChol', 'smoke', 'stroke', 'diabetes', 'physHlth', 'diffWalk', 'age']]
+
+        # binary_df = pd.DataFrame(binary_features, columns=['highBloodPressure', 'highChol', 'smoke', 'stroke', 'diabetes', 'diffWalk'])
+        # non_binary_df = pd.DataFrame(non_binary_features_scaled, columns=['physHlth', 'age'])
+        
+        # input_data_df = pd.concat([binary_df, non_binary_df], axis=1)
+        # columns = ['highBloodPressure', 'highChol', 'smoke', 'stroke', 'diabetes', 'physHlth', 'diffWalk', 'age']
+        # input_data_df = input_data_df[column_order]
+        # input_data_scaled = input_data_df.values
+
+
+
+        # input_data = np.array([[highBloodPressure, highChol, data.smoke, data.stroke, data.diabetes, data.physHealth, data.diffWalk, data.age]])
+        # # bp, col, smoke, stroke
+        # input_data_scaled = scalar.transform(input_data)
+
+
+        prediction = model.predict(df)
 
         return {"prediction": int(prediction[0]), "high_bp": int(highBloodPressure), "high_chol": int(highChol) }
 
